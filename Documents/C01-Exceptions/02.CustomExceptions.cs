4334fb5ffb7cb9c1eabb0d796c1cb39cb2f1420b
Veaceslav Chirilov <Veaceslav.Chirilov@endava.com>
2019-02-27 15:48:40 +0200

    Custom Exceptions

--- Documents/C01-Exceptions/02.CustomExceptions.cs
+++ Documents/C01-Exceptions/02.CustomExceptions.cs
@@ -1,3 +1,47 @@
 //**************************************************************************************************
-//Exception block
+//Developer can create its own exception classes and use these exceptions in application.
+//The parent class for all custom exceptions must be Exception or any other exception classes.
 //**************************************************************************************************
+
+class PersonException : ArgumentException
+{
+    public int Value { get;}
+    public PersonException(string message, int val): base(message)
+    {
+        Value = val;
+    }
+}
+
+
+class Person
+{
+    public string Name { get; set; }
+    private int age;
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value < 18)
+                throw new PersonException("Persons younger than 18 years cannot register", value);
+            else
+                age = value;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Person p = new Person { Name = "Tom", Age = 13 };
+        }
+        catch (PersonException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine($"Invalid value: {ex.Value}");
+        }
+        Console.Read();
+    }
+}
