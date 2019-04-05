7930dee392f54600aec83d06ab0db2faad5417b7
Veaceslav Chirilov <Veaceslav.Chirilov@endava.com>
2019-04-05 19:18:06 +0300

    Added Razor

--- Documents/C09-ASP.NET MVC/C03-Views/C01.Razor.cs
+++ Documents/C09-ASP.NET MVC/C03-Views/C01.Razor.cs
@@ -1,4 +1,45 @@
 /****************************************************************************
-Every request that comes to your application is handled by a controller.
-Controllers are responsible for processing incoming requests, performing operations on the domain model, and selecting views to render to the user.
+
 *****************************************************************************/
+
+public ActionResult Index()
+{
+	ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+	ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+	string message = "This is an HTML element: <input>";
+	return View((object)message);
+}
+
+
+
+@model string
+@{
+    Layout = null;
+}
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Index</title>
+</head>
+<body>
+    <div>
+        Here are the fruits:
+        @foreach (string str in (string[])ViewBag.Fruits)
+        {
+            <b>@str </b>
+        }
+    </div>
+    <div>
+        Here are the cities:
+        @foreach (string str in (string[])ViewBag.Cities)
+        {
+            <b>@str </b>
+        }
+    </div>
+    <div>
+        Here is the message:
+        <p>@Model</p>
+    </div>
+</body>
+</html>
