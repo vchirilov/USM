75fce6ef82a3b4516e84182f983848c633557271
Veaceslav Chirilov <Veaceslav.Chirilov@endava.com>
2019-04-09 17:07:58 +0300

    Added SportsStore application

---
+++ Documents/C09-ASP.NET MVC/C04-SportsStore/C01-CreateApplication.cs
@@ -0,0 +1,7 @@
+/****************************************************************************
+1. Create solution with name SportsStore
+2. Add an ASP.NET MVC Web Application project (Basic template) called SportsStore.WebUI
+3. Add a new class library project called SportsStore.Domains
+4. Add an unit test project called SportsStore.UnitTests
+5. Add references between projects
+*****************************************************************************/
