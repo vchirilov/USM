3ef08a0e0d5b9c7151c5e6dc89852157f86a16de
Veaceslav Chirilov <Veaceslav.Chirilov@endava.com>
2019-04-09 17:33:57 +0300

    Created and registered dependency resolver

--- USM.ASP.NET-MVC/SportsStore/SportsStore.WebUI/Global.asax.cs
+++ USM.ASP.NET-MVC/SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportsStore.WebUI.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,8 @@ namespace SportsStore.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
         }
     }
 }
---
+++ USM.ASP.NET-MVC/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver()
+        {
+            Kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public IKernel Kernel { get => kernel; set => kernel = value; }
+
+        public object GetService(Type serviceType)
+        {
+            return Kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return Kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            //kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
