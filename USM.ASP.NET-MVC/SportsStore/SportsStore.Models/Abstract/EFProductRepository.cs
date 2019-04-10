030e11fa05609020bdb0d7624d035b8f4539b5d4
Veaceslav Chirilov <Veaceslav.Chirilov@endava.com>
2019-04-10 16:54:38 +0300

    Added EntityFramework

---
+++ USM.ASP.NET-MVC/SportsStore/SportsStore.Models/Abstract/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using SportsStore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models.Abstract
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private SportsStoreContext context = new SportsStoreContext();
+
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
---
+++ USM.ASP.NET-MVC/SportsStore/SportsStore.Models/SportsStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using SportsStore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class SportsStoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
